706eb4c8377fcf89fd309287119e368e37eb692b
Heinzmann Robert (SupplyOn) <Robert.Heinzmann@supplyon.com>
2019-05-13 15:02:07 +0200

    Fix Nullpointer Exception with Debug logging enabled and empty CORS Origin List

--- services/src/main/java/org/keycloak/services/resources/Cors.java
+++ services/src/main/java/org/keycloak/services/resources/Cors.java
@@ -190,7 +190,11 @@ public class Cors {
 
         if (!preflight && (allowedOrigins == null || (!allowedOrigins.contains(origin) && !allowedOrigins.contains(ACCESS_CONTROL_ALLOW_ORIGIN_WILDCARD)))) {
             if (logger.isDebugEnabled()) {
-                logger.debugv("Invalid CORS request: origin {0} not in allowed origins {1}", origin, allowedOrigins);
+                if (allowedOrigins == null) {
+                    logger.debugv("Invalid CORS request: list of allowed origins is empty (origin {0})", origin);
+                } else {
+                    logger.debugv("Invalid CORS request: origin {0} not in allowed origins {1}", origin, allowedOrigins);
+                }
             }
             return;
         }
